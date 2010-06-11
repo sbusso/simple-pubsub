9df4d364daa08452eefddf819e3b38ff4b1a64ef
Stephane Busso <stephane.busso@gmail.com>
2010-06-11 13:14:41 +0200

    ensure publisher

--- ruby-publisher/lib/publisher.rb
+++ ruby-publisher/lib/publisher.rb
@@ -2,24 +2,40 @@ require 'rubygems'
 require 'json' # 'msgpack'
 require 'socket'
 #require 'eventmachine'
-require 'pp'
-
+require 'logger'
 
 class Publisher
   
+  attr :state
+  
+  def logger
+    @logger ||= defined? Rails ? Rails.logger : Logger.new(STDOUT)
+  end
+  
   def initialize(addr = '127.0.0.1', port = 8081)
-    @socket = TCPSocket.new(addr, port)
+    @state = :disconnected
+    begin
+      @socket = TCPSocket.new(addr, port)
+      @state = :connected
+    rescue
+      logger.debug "Publisher connexion failed"
+    end
   end
 
   def publish(channel, opts)
-    # EventMachine::run do
-    #   EventMachine::connect(addr, port, Publisher).instance_eval(&block)
-    # end
-    request = {:channel => channel}.merge(opts).to_json+ '\ufffd'
-    @socket.print request
+    if connected?
+      request = {:channel => channel}.merge(opts).to_json+ '\ufffd'
+      @socket.print request
+    else
+      logger.debug "The publisher is not connected"
+    end
+  end
+  
+  def connected?
+    return @state == :connected
   end
   
   def destroy
-    @socket.close
+    @socket.close if connected?
   end
 end
